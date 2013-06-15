95aa96b2dc4c85e05754a422f0622c0e13abbf08
Chad Remesch <chad@remesch.com>
2013-06-14 22:48:57 -0400

    Compatibility with latest Tribe.

--- lib/tribe_em_amfsocket/actor_proxy.rb
+++ lib/tribe_em_amfsocket/actor_proxy.rb
@@ -1,6 +1,6 @@
 # Attempt to provide thread-safe communication between EventMachine and AMF Socket.
 # 1. Always use EM.schedule to push work to the reactor thread.
-# 2. Always use @actor.message! to push work to the actor's thread pool.
+# 2. Always use @actor.deliver_message! to push work to the actor's thread pool.
 
 module Tribe
   module EM
@@ -24,17 +24,17 @@ module Tribe
         def post_init
           super
 
-          @actor.message!(:post_init, nil)
+          @actor.deliver_message!(:post_init, nil)
         end
 
         def unbind
           super
 
-          @actor.message!(:unbind, nil)
+          @actor.deliver_message!(:unbind, nil)
         end
 
         def receive_message(message)
-          @actor.message!(:receive_message, message)
+          @actor.deliver_message!(:receive_message, message)
         end
 
         #
